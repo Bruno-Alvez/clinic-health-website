796e2e842ee80cbe6231aab3f2b268c27583847e
Bruno-Alvez <bruno.bsantos75@gmail.com>
2025-07-25 23:13:27 -0300

    feat: setup-initial-project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-white text-gray-900 flex items-center justify-center">
+      <h1 className="text-3xl font-semibold">Clínica Health</h1>
+    </main>
+  );
+}
+
+export default App;
